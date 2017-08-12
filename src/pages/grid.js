c78a8a34034f1a121e1f9f9437659d0c8600c0d7
MicahMonfrini <ffrock905@gmail.com>
2017-08-11 19:42:22 -0500

    grid created

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -4,17 +4,21 @@ import Head from 'next/head';
 
 function GridPage() {
   return (
-    <div>
+    <div className="grid">
       <Head>
         <title>My styled page</title>
         <link href="/static/grid.css" rel="stylesheet" />
       </Head>
-      <h1 className="test_class">
-        CSS GRID WOOO
-      </h1>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+      <div>Box 1</div>
+      <div>Box 2</div>
+      <div>Box 3</div>
+      <div>Box 4</div>
+      <div>Box 5</div>
+      <div>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </div>
     </div>
   );
 }
